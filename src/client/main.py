7e6eeeabfaf5c8ac626cd0420f263da1d33b31c9
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-05-12 16:08:47 -0300

    tmp: make client sleep so that gateway gets ready

--- src/client/main.py
+++ src/client/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from enum import Enum
 from socket import AF_INET, SOCK_STREAM, socket
 
@@ -10,31 +11,36 @@ GATEWAY_PORT = os.environ["GATEWAY_PORT"]
 
 BUF_SIZE = 1024
 
+
 class ResponseType(Enum):
     FIN = 0
+
 
 def get_response_type(response: bytes):
     return response[0]
 
+
 def main():
+    # TODO: esto lo dejo acá porque me trabé haciendo q se ejecute bien el script de healthcheck
+    time.sleep(1)
     skt = socket(AF_INET, SOCK_STREAM)
     skt.connect((GATEWAY_HOST, int(GATEWAY_PORT)))
 
     transactions_batch = []
-    with open(TRANSACTIONS_PATH, 'r') as transactions:
+    with open(TRANSACTIONS_PATH, "r") as transactions:
         line = transactions.readline()
         transactions_batch.append(line)
 
     accounts_batch = []
-    with open(ACCOUNTS_PATH, 'r') as accounts:
+    with open(ACCOUNTS_PATH, "r") as accounts:
         line = accounts.readline()
         accounts_batch.append(line)
 
     for t in transactions_batch:
-       skt.sendall(t.encode())
+        skt.sendall(t.encode())
 
     for t in accounts_batch:
-       skt.sendall(t.encode())
+        skt.sendall(t.encode())
 
     responses = []
     while True:
@@ -45,10 +51,10 @@ def main():
 
     skt.close()
 
-    with open(RESPONSES_PATH, 'w') as file:
+    with open(RESPONSES_PATH, "w") as file:
         for r in responses:
             file.write(r)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
