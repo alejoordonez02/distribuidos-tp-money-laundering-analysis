d7f76ee1c1cec414e9e1ae07363b724247e63af2
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-05-16 21:41:06 -0300

    feat: actually join msgs per-client in dummy join (not so dummy anymore)

--- src/join/join_fns/dummy_join.py
+++ src/join/join_fns/dummy_join.py
@@ -7,13 +7,21 @@ from .join_fn import JoinFn
 
 class DummyJoin(JoinFn):
     def __init__(self):
-        self.transactions: Transactions = Transactions([])
+        self.client_transactions: dict[UUID, Transactions] = {}
 
     def join(self, el: Transactions):  # type: ignore[reportIncompatibleMethodOverride]
-        self.transactions.transactions.extend(el.transactions)
+        if el.client_id not in self.client_transactions:
+            self.client_transactions[el.client_id] = Transactions(el.client_id, [])
+
+        self.client_transactions[el.client_id].transactions.extend(el.transactions)
 
     def get_response(self, client_id: UUID) -> Response:
-        body = f"{[t.__dict__ for t in self.transactions.transactions]}"
+        if client_id not in self.client_transactions:
+            self.client_transactions[client_id] = Transactions(client_id, [])
+
+        body = (
+            f"{[t.__dict__ for t in self.client_transactions[client_id].transactions]}"
+        )
         response = Response(client_id, body)
 
         return response
