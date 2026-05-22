30a41bce5c98e0ae4064457b4e6682c786ddabb9
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-05-21 22:57:22 -0300

    refactor: let join run in one node

--- src/join/main.py
+++ src/join/main.py
@@ -7,9 +7,11 @@ from common.comms.middleware import QueueRabbitMQ
 from join import Join
 
 MOM_HOST = os.environ["MOM_HOST"]
-CLIENT_RESPONSES_RX = os.environ["CLIENT_RESPONSES_RX"]
-CLIENT_RESPONSES_TX = os.environ["CLIENT_RESPONSES_TX"]
-STRATEGY = os.getenv("STRATEGY", "uc1")
+UC1_RX = os.environ["UC1_RX"]
+UC2_RX = os.environ["UC2_RX"]
+UC3_RX = os.environ["UC3_RX"]
+UC5_RX = os.environ["UC5_RX"]
+RESPONSES_TX = os.environ["RESPONSES_TX"]
 
 LOGGING_LEVEL = os.getenv("LOGGING_LEVEL", "INFO")
 
@@ -18,21 +20,15 @@ def main():
     logging.basicConfig(level=LOGGING_LEVEL)
     logging.getLogger("pika").setLevel(logging.WARNING)
 
-    match STRATEGY:
-        case "uc1":
-            join_fn = UC1Join()
-        case "uc2":
-            join_fn = UC2Join()
-        case "uc5":
-            join_fn = UC5Join()
-        case _:
-            raise ValueError(f"unknown join strategy: {STRATEGY}")
+    partial_res_handlers = [
+        (QueueRabbitMQ(MOM_HOST, UC1_RX), UC1Join()),
+        (QueueRabbitMQ(MOM_HOST, UC2_RX), UC2Join()),
+        # TODO: (QueueRabbitMQ(MOM_HOST, UC3_RX), UC3Join()),
+        (QueueRabbitMQ(MOM_HOST, UC5_RX), UC5Join()),
+    ]
+    responses_tx = QueueRabbitMQ(MOM_HOST, RESPONSES_TX)
 
-    partial_res_handlers = [(QueueRabbitMQ(MOM_HOST, CLIENT_RESPONSES_RX), join_fn)]
-    responses_tx = QueueRabbitMQ(MOM_HOST, CLIENT_RESPONSES_TX)
-
-    # NOTE: no sé por qué me tira error el linter acá...
-    join = Join(partial_res_handlers, responses_tx)  # type: ignore[reportArgumentType]
+    join = Join(partial_res_handlers, responses_tx)
     join.start()
 
 
