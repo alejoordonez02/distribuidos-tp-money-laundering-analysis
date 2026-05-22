d5d5b89175be04ed64ed1caf220eee1e6f5fdd51
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-05-22 00:16:14 -0300

    feat: add uc4 join

--- src/join/join_fns/__init__.py
+++ src/join/join_fns/__init__.py
@@ -2,4 +2,5 @@ from .dummy_join import DummyJoin
 from .join_fn import JoinFn
 from .uc1_join import UC1Join
 from .uc2_join import UC2Join
+from .uc4_join import UC4Join
 from .uc5_join import UC5Join
