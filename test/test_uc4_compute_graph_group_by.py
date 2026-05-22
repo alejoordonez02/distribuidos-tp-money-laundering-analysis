e319b302f1916f42e254337033a0c202f384fc63
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-05-22 16:47:53 -0300

    test: uc4 compute two path graph

--- test/test_uc4_compute_graph_group_by.py
+++ test/test_uc4_compute_graph_group_by.py
@@ -18,6 +18,21 @@ def _transaction(from_bank: str, from_account: str, to_bank: str, to_account: st
     )
 
 
+def _print_nodes(graph: Graph) -> str:
+    nodes_str = ""
+    for n, (predecessors, successors) in graph.nodes.items():
+        nodes_str += f"--- node: {n.bank}-{n.account} ---\n"
+        nodes_str += "\t--- predecessors: ---\n"
+        for p in predecessors:
+            nodes_str += f"\t- p: {p.bank}-{p.account}\n"
+
+        nodes_str += "\t--- successors: ---\n"
+        for s in successors:
+            nodes_str += f"\t- p: {s.bank}-{s.account}\n"
+
+    return nodes_str
+
+
 def test_single_path_graph():
     some_uuid = "some_uuid"
     origin_bank = "origin_bank"
@@ -65,4 +80,74 @@ def test_single_path_graph():
 
     graph = fn.get_result(some_uuid)  # type: ignore[reportArgumentType]
 
-    assert graph == expected_graph
+    assert graph == expected_graph, (
+        f"expected:\n{_print_nodes(expected_graph)}\ngot:\n{_print_nodes(graph)}"
+    )
+
+
+def test_two_path_graph():
+    some_uuid = "some_uuid"
+    origin_bank = "origin_bank"
+    origin_account = "origin_account"
+    middle_bank = "middle_bank"
+    middle_account = "middle_account"
+    middle_bank2 = "middle_bank2"
+    middle_account2 = "middle_account2"
+    destination_bank = "destination_bank"
+    destination_account = "destination_account"
+
+    transactions = Transactions(
+        some_uuid,  # type: ignore[reportArgumentType]
+        [
+            # scatter transactions
+            _transaction(
+                from_bank=origin_bank,
+                from_account=origin_account,
+                to_bank=middle_bank,
+                to_account=middle_account,
+            ),
+            _transaction(
+                from_bank=origin_bank,
+                from_account=origin_account,
+                to_bank=middle_bank2,
+                to_account=middle_account2,
+            ),
+            # gather transactions
+            _transaction(
+                from_bank=middle_bank,
+                from_account=middle_account,
+                to_bank=destination_bank,
+                to_account=destination_account,
+            ),
+            _transaction(
+                from_bank=middle_bank2,
+                from_account=middle_account2,
+                to_bank=destination_bank,
+                to_account=destination_account,
+            ),
+        ],
+    )
+
+    origin_node = Node(origin_bank, origin_account)
+    middle_node = Node(middle_bank, middle_account)
+    middle_node2 = Node(middle_bank2, middle_account2)
+    destination_node = Node(destination_bank, destination_account)
+
+    expected_graph = Graph(
+        some_uuid,  # type: ignore[reportArgumentType]
+        {
+            origin_node: (set(), set([middle_node, middle_node2])),
+            destination_node: (set([middle_node, middle_node2]), set()),
+            middle_node: (set([origin_node]), set([destination_node])),
+            middle_node2: (set([origin_node]), set([destination_node])),
+        },
+    )
+
+    fn = UC4ComputeGraph()
+    fn.group_by(transactions)
+
+    graph = fn.get_result(some_uuid)  # type: ignore[reportArgumentType]
+
+    assert graph == expected_graph, (
+        f"expected:\n{_print_nodes(expected_graph)}\ngot:\n{_print_nodes(graph)}"
+    )
