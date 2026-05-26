f3beb549f2903990fe187aa873bd14791091444b
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-05-26 16:19:08 -0300

    feat: ring rabbitmq middleware

--- src/common/comms/middleware/ring_rabbitmq.py
+++ src/common/comms/middleware/ring_rabbitmq.py
@@ -1,72 +1,118 @@
-from typing import Callable, Hashable
+from typing import Callable
 
-from pika import BlockingConnection, ConnectionParameters
-from pika.exceptions import AMQPConnectionError
+from .errors import MOMCloseError
+from .exchange_mom import MOMExchange
+from .exchange_rabbitmq import ExchangeRabbitMQ
+from .ring_mom import MOMRing, MOMRingError
 
-from .errors import (
-    MOMDisconnectedError,
-    MOMMessageError,
-)
-from .ring_mom import MOMRing
+
+def _get_front_back_ids(self_id: int, peer_ids: list[int]) -> tuple[int, int]:
+    if self_id in peer_ids:
+        raise MOMRingError("invalid peer ids list, self's id should not be included")
+
+    npeers = len(peer_ids)
+    if not npeers:
+        return self_id, self_id
+    elif npeers == 1:
+        return peer_ids[0], peer_ids[0]
+
+    for idx, p in enumerate(sorted(peer_ids)):
+        if p > self_id:
+            back_idx = (npeers + idx - 1) % npeers
+            front_id = peer_ids[idx]
+            back_id = peer_ids[back_idx]
+
+            return front_id, back_id
+
+    raise MOMRingError(
+        "invalid peer ids list, could not find proper back and next peers"
+    )
 
 
 class RingRabbitMQ(MOMRing):
-    def __init__(self, self_id: Hashable, peer_ids: list[Hashable]):
-        self.exchange_name = exchange_name
-        self.conn = BlockingConnection(ConnectionParameters(host))
-        self.chan = self.conn.channel()
-        self.chan.exchange_declare(exchange=exchange_name)
+    def __init__(
+        self,
+        host: str,
+        ring_name: str,
+        self_id: int,
+        peer_ids: list[int],
+        exchange_factory: Callable[
+            [str, str, list[str]], MOMExchange
+        ] = ExchangeRabbitMQ,
+    ):
+        """
+        Create a RingRabbitMQ middleware.
 
-        queue = self.chan.queue_declare(queue="", exclusive=True)
-        queue_name = queue.method.queue
-        self.queue_name = queue_name
-        self.routing_keys = routing_keys
-        for k in routing_keys:
-            self.chan.queue_bind(
-                exchange=exchange_name, queue=queue_name, routing_key=k
-            )
+        # Args
+        * `host` - the broker host.
+        * `ring_name` - the name for the ring cluster.
+        * `self_id` - the id of *this* node.
+        * `peer_ids` - the ids of the peer nodes, where `self_id`
+          is **not** included.
+        * `exchange_factory` - (optional) a method for creating the exchange
+          to be used. *This optional parameter could be removed from the
+          API*.
+
+        # Returns
+        A new `RingRabbitMQ` middleware.
+        """
+        self.id = self_id
+        self.front_id, self.back_id = _get_front_back_ids(self.id, peer_ids)
+
+        self.exchange_front = exchange_factory(host, ring_name, [str(self.front_id)])
+        self.exchange_back = exchange_factory(host, ring_name, [str(self.back_id)])
 
     def start_consuming(
         self, on_message_callback: Callable[[bytes, Callable, Callable], None]
-    ) -> None:
-        def callback(chan, method, _, body):
-            on_message_callback(
-                body, lambda: self._ack(chan, method), lambda: self._nack(chan, method)
-            )
+    ):
+        """
+        Starts consuming messages from the back node through the broker in
+        a blocking manner and handles them with the passed message handler
+        in handle.
 
-        self.chan.basic_consume(queue=self.queue_name, on_message_callback=callback)
+        # Args
+        * `on_message_callback(msg_bytes, ack, nack)` - the message handling
+          function.
+
+        # Errors
+        * `MOMDisconnectedError` if the connection with the broker is lost.
+        * `MOMMessageError` if an unexpected error occurs.
+        """
+
+        self.exchange_back.start_consuming(on_message_callback)
+
+    def stop_consuming(self):
+        """
+        Stop consuming messages from back peer through the broker.
+        """
+        self.exchange_back.stop_consuming()
+
+    def send(self, message: bytes):
+        """
+        Send a message to front peer through the broker.
+
+        # Args
+         * `message` - the bytes of the serialized message to be sent.
+
+        # Errors
+         * `MOMDisconnectedError` if the connection with the broker is lost.
+         * `MOMMessageError` if an unexpected error occurs.
+        """
+        self.exchange_front.send(message)
+
+    def close(self):
         try:
-            self.chan.start_consuming()
-        except AMQPConnectionError as e:
-            raise MOMDisconnectedError(str(e)) from e
-        except Exception as e:
-            raise MOMMessageError(str(e)) from e
+            self.exchange_front.close()
+        except MOMCloseError as e:
+            raise MOMCloseError(
+                f"failed to close connection with front peer (id: {self.front_id})",
+                str(e),
+            ) from e
 
-    def stop_consuming(self) -> None:
         try:
-            self.chan.stop_consuming()
-        except AMQPConnectionError as e:
-            raise MOMDisconnectedError(str(e)) from e
-
-    def send(self, message: bytes) -> None:
-        for k in self.routing_keys:
-            try:
-                self.chan.basic_publish(
-                    exchange=self.exchange_name, routing_key=k, body=message
-                )
-            except AMQPConnectionError as e:
-                raise MOMDisconnectedError(str(e)) from e
-            except Exception as e:
-                raise MOMMessageError(str(e)) from e
-
-    def close(self) -> None:
-        try:
-            self.conn.close()
-        except Exception as e:
-            raise MOMMessageError(str(e)) from e
-
-    def _ack(self, chan, method) -> None:
-        chan.basic_ack(delivery_tag=method.delivery_tag)
-
-    def _nack(self, chan, method) -> None:
-        chan.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+            self.exchange_back.close()
+        except MOMCloseError as e:
+            raise MOMCloseError(
+                f"failed to close connection with back peer (id: {self.back_id})",
+                str(e),
+            ) from e
