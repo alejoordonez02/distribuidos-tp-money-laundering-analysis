6be9a7dba40269e740d16d19b6aed3a70b3902f0
lminervino18 <lminervino@fi.uba.ar>
2026-05-26 13:57:58 -0300

    test(cfg): raise transaction sample size to 3M

--- test/cfg.py
+++ test/cfg.py
@@ -1,6 +1,6 @@
 TRANSACTIONS_PATH = "datasets/LI-Small_Trans.csv"
 ACCOUNTS_PATH = "datasets/LI-Small_accounts.csv"
-TRANSACTIONS_SAMPLE_SIZE = 2000000 # whole dataset
+TRANSACTIONS_SAMPLE_SIZE = 3000000
 ACCOUNTS_SAMPLE_SIZE = None  # whole dataset
 
 NCLIENTS = 1
