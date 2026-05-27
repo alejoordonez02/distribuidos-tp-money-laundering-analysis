e2645ca75006b9d903b3f06dfd0923e3226c8400
lminervino18 <lminervino@fi.uba.ar>
2026-05-27 12:05:21 -0300

    chore(test): bump transaction sample size to 4m

--- test/cfg.py
+++ test/cfg.py
@@ -3,7 +3,7 @@ ACCOUNTS_PATH = "datasets/LI-Small_accounts.csv"
 ACCOUNTS_SAMPLE_SIZE = None  # whole dataset
 
 NCLIENTS = 8
-TRANSACTIONS_SAMPLE_SIZE: int | None = 1000000  # None = whole dataset; divided by NCLIENTS per client
+TRANSACTIONS_SAMPLE_SIZE: int | None = 4000000  # None = whole dataset; divided by NCLIENTS per client
 CLIENT_DATASETS_PATH = "datasets/"
 CLIENT_EXPECTED_RESPONSES_PATH = "test/expected_responses/"
 
