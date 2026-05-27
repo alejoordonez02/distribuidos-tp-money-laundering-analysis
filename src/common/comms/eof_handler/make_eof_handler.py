0a525335c70f6f94c718d9d04f9d2311133ea628
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-05-27 18:57:36 -0300

    refactor: raise error if npeers is less than one in make eof handler

--- src/common/comms/eof_handler/make_eof_handler.py
+++ src/common/comms/eof_handler/make_eof_handler.py
@@ -1,9 +1,12 @@
 import os
+from queue import Queue
 
+from common.comms.eof_handler.stateful_ring_eof_handler import StatefulRingEOFHandler
+from common.comms.messages.eof import EOF
 from common.comms.middleware import MOMQueue, RingRabbitMQ
 
-from .eof_handler import StatelessEOFHandler
-from .single_node_eof_handler import SingleNodeEOFHandler
+from .eof_handler import StatefulEOFHandler, StatelessEOFHandler
+from .single_node_eof_handler import SingleNodeEOFHandler, StatefulSingleNodeEOFHandler
 from .stateless_ring_eof_handler import StatelessRingEOFHandler
 
 
@@ -24,6 +27,8 @@ def make_stateless_eof_handler(
 
     NPEERS = int(os.getenv("NPEERS", "1"))
 
+    if NPEERS < 1:
+        raise ValueError("NPEERS must be greater or equal than 1")
     if NPEERS == 1:
         return SingleNodeEOFHandler(txs)
 
@@ -34,3 +39,36 @@ def make_stateless_eof_handler(
     mom_ring = RingRabbitMQ(mom_host, RING_NAME, IDX, peer_ids)
 
     return StatelessRingEOFHandler(mom_ring, txs)
+
+
+def make_stateful_eof_handler(
+    mom_host: str, txs: list[MOMQueue], internal_eofs_tx: Queue[EOF]
+) -> StatefulEOFHandler:
+    """
+    Create a stateful end of file message handler.
+
+    # Args
+    * `mom_host` - the host of the mom broker.
+    * `txs` - the write halves for forwarding end of file messages
+      once they have already been processed.
+    * `internal_eofs_tx` - the medium through which the handler
+      will communicate the receipt of cluster end of file messages.
+
+    # Returns
+    A new `StatefulEOFHandler`
+    """
+
+    NPEERS = int(os.getenv("NPEERS", "1"))
+
+    if NPEERS < 1:
+        raise ValueError("NPEERS must be greater or equal than 1")
+    if NPEERS == 1:
+        return StatefulSingleNodeEOFHandler(txs, internal_eofs_tx)
+
+    IDX = int(os.environ["IDX"])
+    RING_NAME: str = os.environ["RING_NAME"]
+
+    peer_ids = [idx for idx in range(NPEERS) if idx != IDX]
+    mom_ring = RingRabbitMQ(mom_host, RING_NAME, IDX, peer_ids)
+
+    return StatefulRingEOFHandler(IDX, mom_ring, txs, internal_eofs_tx)
