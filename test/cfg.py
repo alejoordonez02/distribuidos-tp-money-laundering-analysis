e32c35d0c4d5c2903088276bdca87b790320b363
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-05-28 18:42:53 -0300

    tmp: set test cfg to whole small transactions with eight clients

--- test/cfg.py
+++ test/cfg.py
@@ -2,8 +2,8 @@ TRANSACTIONS_PATH = "datasets/LI-Small_Trans.csv"
 ACCOUNTS_PATH = "datasets/LI-Small_accounts.csv"
 ACCOUNTS_SAMPLE_SIZE = None  # whole dataset
 
-NCLIENTS = 5
-TRANSACTIONS_SAMPLE_FRAC: float = 0.057770  # ~400k per client → 2M total across 5 clients
+NCLIENTS = 8
+TRANSACTIONS_SAMPLE_FRAC: float = 1 / NCLIENTS
 CLIENT_DATASETS_PATH = "datasets/"
 CLIENT_EXPECTED_RESPONSES_PATH = "test/expected_responses/"
 
