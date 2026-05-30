031887e6e5c51a308284fd473d3716e1d995a48d
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-05-30 08:48:53 -0300

    refactor: force groupby fn subclasses to impl abstract methods
    me lo había olvidado

--- src/group_by/group_by_fns/group_by_fn.py
+++ src/group_by/group_by_fns/group_by_fn.py
@@ -1,10 +1,9 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from typing import Iterable
 
 from common.comms.messages import Message
 
 
-class GroupByFn:
+class GroupByFn(ABC):
     @abstractmethod
-    def group_by(self, msg: Message) -> Iterable[tuple[Message, int]]:
-        pass
+    def group_by(self, msg: Message) -> Iterable[tuple[Message, int]]: ...
