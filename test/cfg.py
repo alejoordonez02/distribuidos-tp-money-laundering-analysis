6d33cddcad9ffe81a35c1ca67ecf8a740ddcc325
lminervino18 <lminervino@fi.uba.ar>
2026-06-03 20:18:40 -0300

    chore: select HI-Medium dataset for the run

--- test/cfg.py
+++ test/cfg.py
@@ -1,12 +1,14 @@
 # TRANSACTIONS_PATH = "datasets/synthetic_uc4_50k.csv"
-TRANSACTIONS_PATH = "datasets/LI-Small_Trans.csv"
-ACCOUNTS_PATH = "datasets/LI-Small_accounts.csv"
+# TRANSACTIONS_PATH = "datasets/LI-Small_Trans.csv"
+# ACCOUNTS_PATH = "datasets/LI-Small_accounts.csv"
+TRANSACTIONS_PATH = "datasets/HI-Medium_Trans.csv"
+ACCOUNTS_PATH = "datasets/HI-Medium_accounts.csv"
 # TRANSACTIONS_PATH = "datasets/perfect_sample.csv"
 # ACCOUNTS_PATH = "datasets/perfect_sample_accounts.csv"
 ACCOUNTS_SAMPLE_SIZE = None  # whole dataset
 
-NCLIENTS = 2
-TRANSACTIONS_SAMPLE_FRAC: float = 0.5
+NCLIENTS = 1
+TRANSACTIONS_SAMPLE_FRAC: float = 1.0
 CLIENT_DATASETS_PATH = "datasets/"
 CLIENT_EXPECTED_RESPONSES_PATH = "test/expected_responses/"
 
