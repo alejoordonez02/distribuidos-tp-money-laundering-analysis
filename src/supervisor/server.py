df9f77309bd76701cbc925aaa1653ba63c6627f0
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-06-22 04:12:44 -0300

    wip: setup supervisor event loop arch

--- src/supervisor/server.py
+++ src/supervisor/server.py
@@ -1,9 +1,16 @@
+import logging
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from enum import IntEnum
+from queue import Queue
 from socket import AF_INET, SO_REUSEADDR, SOCK_STREAM, SOL_SOCKET, socket
+from threading import Thread
 from typing import Self, Sequence
 
+from common.comms.transport import Connection
+
 from .registry import NodeRegistry
-from .runtime import LeaderRuntime, ReplicaRuntime, SupervisorRuntime
+from .runtime import LeaderDownError, LeaderRuntime, ReplicaRuntime, SupervisorRuntime
 
 
 @dataclass
@@ -20,6 +27,29 @@ class Peer:
         if self.idx is None or (isinstance(other, Peer) and not other.idx):
             raise RuntimeError("cannot compare peer without an assigned idx")
         return self.idx < other.idx if isinstance(other, Peer) else self.idx < other  # type: ignore[reportOperatorIssue]
+
+
+class EventType(IntEnum):
+    LEADER_DOWN = 0
+    PEER_CONNECTION = 1
+
+
+class SupervisorEvent(ABC):
+    @abstractmethod
+    def type(self) -> EventType: ...
+
+
+class LeaderDown(SupervisorEvent):
+    def type(self) -> EventType:
+        return EventType.LEADER_DOWN
+
+
+@dataclass
+class PeerConnection(SupervisorEvent):
+    conn: Connection
+
+    def type(self) -> EventType:
+        return EventType.PEER_CONNECTION
 
 
 class SupervisorNode:
@@ -59,16 +89,59 @@ class SupervisorNode:
 
         # TODO: esto se elige dinámicamente pero ahora sólo quiero ping pong
         leader = max(peers) if len(peers) > 0 and idx < max(peers) else None
-        self._runtime: SupervisorRuntime = (
+        self._runtimes: Queue[SupervisorRuntime] = Queue()
+        self._events: Queue[SupervisorEvent] = Queue()
+
+        self._runtime: SupervisorRuntime
+        runtime = (
             ReplicaRuntime((leader.host, leader_port), ping_delay)
             if leader
             else LeaderRuntime(
                 self._server_listener, self._replica_listener, registry, sweep_interval
             )
         )
+        self._runtimes.put(runtime)
+
+        self._runtime_handle = Thread(target=self._runtime_worker)
+        self._listener_handle = Thread(target=self._listener_worker)
+
+        self._on_election = False
+        self._keep_running = False
 
     def start(self):
-        self._runtime.start()
+        self._keep_running = True
+
+        self._runtime_handle.start()
+        self._listener_handle.start()
+        return self._event_worker()
+
+    def _event_worker(self):
+        while self._keep_running:
+            event = self._events.get()
+            match event.type():
+                case EventType.LEADER_DOWN:
+                    raise NotImplementedError
+
+    def _runtime_worker(self):
+        while self._keep_running:
+            runtime = self._runtimes.get()
+            if not runtime:
+                break
+
+            logging.debug(f"running as {runtime.__class__.__name__}")
+            self._runtime = runtime
+            try:
+                self._runtime.start()
+            except LeaderDownError:
+                self._events.put(LeaderDown())
+
+    def _listener_worker(self):
+        self._node_listener.listen()
+        while self._keep_running:
+            skt, _ = self._node_listener.accept()
+            conn = Connection(skt)
+            self._events.put(PeerConnection(conn))
 
     def stop(self):
+        self._keep_running = False
         self._runtime.stop()
