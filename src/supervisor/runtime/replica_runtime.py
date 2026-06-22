569da1c2fe703017f90d1d6a22f974dd3300bb77
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-06-22 02:25:11 -0300

    feat: replica runtime

---
+++ src/supervisor/runtime/replica_runtime.py
@@ -0,0 +1,69 @@
+import logging
+import time
+from socket import AF_INET, SOCK_STREAM, socket
+
+from common.comms.transport import Connection
+
+from .supervisor_runtime import SupervisorRuntime
+
+
+class LeaderDownError(Exception): ...
+
+
+class ReplicaRuntime(SupervisorRuntime):
+    def __init__(self, leader_addr: tuple[str, int], ping_delay: float):
+        self._leader = Leader(leader_addr)
+        self._keep_running = False
+        self._ping_delay = ping_delay
+
+    def start(self):
+        self._keep_running = True
+        self._run()
+
+    def _run(self):
+        while self._keep_running:
+            time.sleep(self._ping_delay)
+            self._leader.ping_pong()
+
+    def stop(self):
+        self._keep_running = False
+        self._leader.close()
+
+
+class Leader:
+    def __init__(self, leader_addr: tuple[str, int]):
+        self._addr = leader_addr
+        self._conn: Connection | None = None
+        self._keep_running = True
+
+    def _connect(self):
+        skt = socket(AF_INET, SOCK_STREAM)
+        skt.connect(self._addr)
+        self._conn = Connection(skt)
+
+    def ping_pong(self):
+        if not self._conn:
+            self._connect()
+        assert self._conn is not None  # pleasing linter
+
+        try:
+            while not self._keep_running:
+                self._conn.send(b"ping")
+                pong = self._conn.recv()
+                if not pong:
+                    raise LeaderDownError()
+        except OSError as e:
+            logging.debug("lost connection with leader (%s)", e)
+        finally:
+            try:
+                self._conn.close()
+            except OSError:
+                pass
+
+    def close(self):
+        if not self._conn:
+            return
+        try:
+            self._conn.close()
+        except OSError:
+            pass
--- src/supervisor/server.py
+++ src/supervisor/server.py
@@ -1,81 +1,8 @@
-import logging
-import threading
-import time
-from abc import ABC
 from socket import AF_INET, SO_REUSEADDR, SOCK_STREAM, SOL_SOCKET, socket
 from typing import Sequence
 
-from common.comms.supervisor import Heartbeat, Register, decode
-from common.comms.transport import Connection
-
 from .registry import NodeRegistry
-
-
-class SupervisorRuntime(ABC): ...
-
-
-class LeaderRuntime(SupervisorRuntime):
-    def __init__(
-        self,
-        server_listener: socket,
-        replica_listener: socket,
-        registry: NodeRegistry,
-        sweep_interval: float = 0.5,
-    ):
-        self._server_listener = server_listener
-        self._replica_listener = replica_listener
-        self._registry = registry
-        self._sweep_interval = sweep_interval
-
-        self._stop = threading.Event()
-
-    def start(self):
-        self._server_listener.listen()
-        threading.Thread(target=self._sweep, name="sweeper", daemon=True).start()
-        threading.Thread(target=self._accept_loop, name="accept", daemon=True).start()
-
-    def stop(self):
-        self._stop.set()
-        try:
-            self._server_listener.close()
-        except OSError:
-            pass
-
-    def _accept_loop(self):
-        while not self._stop.is_set():
-            try:
-                conn_skt, _ = self._server_listener.accept()
-            except OSError:
-                break
-            threading.Thread(target=self._handle, args=(conn_skt,), daemon=True).start()
-
-    def _handle(self, conn_skt: socket) -> None:
-        conn = Connection(conn_skt)
-        try:
-            while not self._stop.is_set():
-                data = conn.recv()
-                if not data:
-                    break
-                self._dispatch(decode(data))
-        except (OSError, ValueError) as e:
-            logging.debug("supervisor: connection dropped (%s)", e)
-        finally:
-            try:
-                conn.close()
-            except OSError:
-                pass
-
-    def _dispatch(self, msg) -> None:
-        now = time.monotonic()
-        if isinstance(msg, Register):
-            self._registry.register(msg.node_id, msg.kind, now)
-        elif isinstance(msg, Heartbeat):
-            self._registry.heartbeat(msg.node_id, now)
-
-    def _sweep(self) -> None:
-        while not self._stop.is_set():
-            self._registry.check_timeouts(time.monotonic())
-            self._stop.wait(self._sweep_interval)
+from .runtime import LeaderRuntime
 
 
 class SupervisorNode:
